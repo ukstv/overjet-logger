22aae2813e19c659af95ea9a8b524a6467041f09
Sergey Ukustov <ukstv@feather-2.local>
2022-01-10 16:30:07 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { LoggerModes, Formats } from './constants.js';
+export type { ICustomLogger } from './constants.js'
+import Logger from './Logger.js';
+export { Logger }
+export default Logger;
